26b758d7dbb40f8a24e9b53a568093cc41149b52
Dodeca23 <46872357+Dodeca23@users.noreply.github.com>
2021-01-23 10:42:43 +0100

    Added counter display

---
+++ Assets/Scripts/Interface/FrameRateCounter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class FrameRateCounter : MonoBehaviour
+{
+    [Tooltip("Text component of the framerate display")]
+    [SerializeField]
+    private TextMeshProUGUI display = default;
+    [SerializeField, Range(0.1f, 2f)]
+    private float sampleDuration = 1f;
+
+    private int frames;
+    private float duration;
+    private float bestDuration = float.MaxValue;
+    private float worstDuration;
+
+    private void Update()
+    {
+        float frameDuration = Time.unscaledDeltaTime;
+        frames += 1;
+        duration += frameDuration;
+
+        if (frameDuration < bestDuration)
+            bestDuration = frameDuration;
+        if (frameDuration > worstDuration)
+            worstDuration = frameDuration;
+
+        if(duration >= sampleDuration)
+        {
+            display.SetText("FPS\n{0:0}\n{1:0}\n{2:0}", 
+                1f / bestDuration, frames / duration, 1f / worstDuration);
+            frames = 0;
+            duration = 0;
+            bestDuration = float.MaxValue;
+            worstDuration = 0;
+        }
+    }
+}
